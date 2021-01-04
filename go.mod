970349f7cf4b905a56aa9a467ed7371f2bc8440f
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-01-04 14:32:04 -0800

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mod2
+
+go 1.15
+
+require github.com/sebastien-rosset/mod1 v0.0.0-20210104222454-7124816c3934
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/sebastien-rosset/mod1/worker"
+)
+
+func main() {
+  fmt.Println("mod1 Init")
+  worker.Run()
+}
